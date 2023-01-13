704a3bb6247dc327b6bed4f5df9eba30fdb4b972
rahmadanu <sadewarahmadanu@gmail.com>
2023-01-13 17:29:49 +0700

    Init layout palindrome checker activity

---
+++ app/src/main/java/com/suitmedia/suitmediauserapp/ui/first/PalindromeCheckerActivity.kt
@@ -0,0 +1,12 @@
+package com.suitmedia.suitmediauserapp.ui.first
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.suitmedia.suitmediauserapp.R
+
+class PalindromeCheckerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_palindrome_checker)
+    }
+}
